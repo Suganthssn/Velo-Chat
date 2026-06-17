f1353783a2a99faedf6348dcd05b61f6a8be1345
suganth006 <suganth08102006@gmail.com>
2026-06-17 15:27:27 +0530

    fix the bug in get all users

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -32,7 +32,7 @@ export const register = async (req, res, next) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
         });
 
